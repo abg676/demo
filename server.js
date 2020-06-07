c67e8fc70c35b1c6a883027c4ed5b431e92ed4c1
Abhay Goswami <abhay.goswami@precily.com>
2020-06-07 23:25:07 +0530

    add new route for ENV

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use("/", function (req, res) {
   res.render("index");
 });
 
+app.use("/env", function(req, res) {
+	res.send({env: process.env.NODE_ENV})
+});
+
 app.listen(8000, function (err) {
   if (err) console.error("error...");
   console.info("Server is active");
